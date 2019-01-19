74af8980086389275bbd38b15811cbee8fb51293
znemeth <nemeth.z.a@gmail.com>
2019-01-19 20:19:47 +0100

    add 34

--- Week_01/day-5/34/main.cpp
+++ Week_01/day-5/34/main.cpp
@@ -10,15 +10,21 @@ int main(int argc, char* args[]) {
     // Sum: 22, Average: 4.4
     int a;
     int b;
+    int c = 0; //kezdoerteket nem adtam meg & akkor hulyesg jott ki
 
-    std::cout << "Write a number: " << std::endl;
+    std::cout << "Number of numbers: " << std::endl;
     std::cin >>b;
 
     for (int i=0; i<b; i++) {
         std::cout << "Write a number: " << std::endl;
         std::cin >>a;
+        c = c + a;
     };
 
+    int m = c / b;
+
+    std::cout << "sum: " << c << std::endl;
+    std::cout << "mean: " << m << std::endl;
 
     return 0;
 }
