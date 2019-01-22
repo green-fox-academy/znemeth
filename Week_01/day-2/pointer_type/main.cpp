c2012fd4e7caf2075b2995c034083e1f3cd4f0d7
znemeth <nemeth.z.a@gmail.com>
2019-01-22 14:10:55 +0100

    add excersises

--- Week_01/day-2/pointer_type/main.cpp
+++ Week_01/day-2/pointer_type/main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main ()
+{
+    // Create a pointer for each variable and print out the memory addresses of that variables
+    // Print out the values of the pointers
+
+    int a = 31;
+    double b = 432.2;
+    std::string name = "Bob";
+
+    int *a_Ptr = &a;
+    double *b_Ptr = &b;
+    std::string *name_Ptr = &name;
+
+    std::cout << " The value of pointer a: "<< a_Ptr <<std::endl;
+    std::cout << " The value of pointer b: "<< b_Ptr <<std::endl;
+    std::cout << " The value of pointer name: "<< name_Ptr <<std::endl;
+
+    std::cout << " The value of pointer a: "<< *a_Ptr <<std::endl;
+    std::cout << " The value of pointer b: "<< *b_Ptr <<std::endl;
+    std::cout << " The value of pointer name: "<< *name_Ptr <<std::endl;
+
     return 0;
 }
