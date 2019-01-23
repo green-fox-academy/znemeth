a22a4f69509cca6f14cd3a0969e7c6cb4cae4c2e
znemeth <nemeth.z.a@gmail.com>
2019-01-23 13:24:50 +0100

    add find_min

--- Week_02/day-2/find_minimum/main.cpp
+++ Week_02/day-2/find_minimum/main.cpp
@@ -1,23 +1,29 @@
 #include <iostream>
 
+int findMin (int *numb, int size){
+
+    int *ptr = nullptr;
+    int smallval = numb[0];
+
+    for (int i=0; i<size; i++){
+        if (numb[i] < smallval) {
+            smallval = numb[i];
+        }
+    }
+    ptr = &smallval;
+    return *ptr;
+}
+
 int main()
 {
     // Create a function which takes an array (and its length) as a parameter
     // and returns a pointer to its minimum value
 
     int numbers[] = {12, 4, 66, 101, 87, 3, 15};
-
     int size = sizeof(numbers) / sizeof(numbers[0]);
-    int numb = 0;
-    for (int i=0; i<size; i++){
-        if (numbers[i] > numb) {
-            numb = numbers[i];
-        }
-    }
 
-
-    std::cout << "The value of the smallest number is: " << numb << std::endl;
-
+    findMin(numbers, size);
+    std::cout << "The value of the smallest number is: " << findMin(numbers, size) << std::endl;
 
     return 0;
 }
