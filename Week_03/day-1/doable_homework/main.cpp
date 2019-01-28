f03bf7d04a88c5cdce6a661b176b352d2e030409
znemeth <nemeth.z.a@gmail.com>
2019-01-28 20:46:31 +0100

    add doable_homework

---
+++ Week_03/day-1/doable_homework/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(int argc, char* args[]) {
+    std::string t1 = "oak, grey, sixty-three, male";
+    std::string t2 = "pine, brown, seventy-one, male";
+    std::string t3 = "maple, black, eighty-four, male";
+    std::string t4 = "elm, red, ninety-nine, female";
+    std::string t5 = "cypress, blueish gray, forty-six, female";
+
+    return 0;
+}
