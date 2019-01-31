cf33ea971437e5b57897e27593cead38cfbea208
znemeth <nemeth.z.a@gmail.com>
2019-01-31 15:29:10 +0100

    add bunny1

---
+++ Week_03/day-4/bunny1/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+// We have a number of bunnies and each bunny has two big floppy ears.
+// We want to compute the total number of ears across all the bunnies recursively (without loops or multiplication).
+
+int adder (int x) {
+    if (x < 1) {
+        return 0;
+    } else {
+        return 2 + adder(x-1);
+    }
+}
+
+int main() {
+      int bunny = 1;
+      adder(bunny);
+      std::cout << "Number: " << adder(bunny) << std::endl;
+      return 0;
+}
