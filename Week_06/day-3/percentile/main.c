866f872aa9e1fa7b491bee26e67773cb2c7cebdd
znemeth <nemeth.z.a@gmail.com>
2019-02-27 16:33:19 +0100

    add percent_1

---
+++ Week_06/day-3/percentile/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Read the size of a matrix(don't have to be squared matrix) from the console.
+//Allocate memory dynamically for the elements of the matrix, then read the elements.
+//Create a function which can calculate the n-th percentile of the elements.
+//Calculate the 80-th and the 90-th percentile.
+
+int main()
+{
+    return 0;
+}
