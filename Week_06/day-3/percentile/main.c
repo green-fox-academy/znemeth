110ee033f6ad22f7e84c532cc0653cd1dd5990c9
znemeth <nemeth.z.a@gmail.com>
2019-02-27 16:54:31 +0100

    add percent_2

--- Week_06/day-3/percentile/main.c
+++ Week_06/day-3/percentile/main.c
@@ -8,5 +8,30 @@
 
 int main()
 {
+    int m;
+    int n;
+
+    printf("Number of rows and columns of the matrix: \n");
+    scanf("%d %d", &m, &n);
+
+    int **matrix = (int **) malloc(m * sizeof(int *));
+
+    for (int i = 0; i < m; i++) {
+        matrix[i] = (int *) malloc(m * sizeof(int));
+    }
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
     return 0;
 }
