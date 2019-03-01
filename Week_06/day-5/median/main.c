3976d30c694ebf95517b68accaf3658cbd2c8e68
znemeth <nemeth.z.a@gmail.com>
2019-03-01 17:20:45 +0100

    add median

---
+++ Week_06/day-5/median/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+// Every line contains the name of the group and after a colon, the height of people in the group separated by commas
+// Find the group where the median is the lowest.
+// Note that the groups do not necessarily contain the same amount of people
+
+int main()
+{
+    return 0;
+}
