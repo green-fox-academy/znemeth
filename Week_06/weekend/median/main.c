aec01993ea2ef59004cd0ff65170b46fdabb6584
znemeth <nemeth.z.a@gmail.com>
2019-03-02 13:47:15 +0100

    add weekend_files_update_2

--- Week_06/weekend/median/main.c
+++ Week_06/weekend/median/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <malloc.h>
+#include <mem.h>
 
 // Every line contains the name of the group and after a colon, the height of people in the group separated by commas
 // Find the group where the median is the lowest.
@@ -6,5 +8,27 @@
 
 int main()
 {
+    FILE *fp = fopen("inputfile.txt", "r");
+/*
+    if ((fp = fopen("inputfile.txt", "r")) == NULL) {
+        printf("Can't open file");
+        exit(1);
+    }
+*/
+    char str_out[100][100];
+    while ((getc(fp)) != EOF)
+        for (int i = 0; i < 10; i++) {
+            fscanf(fp, "%s\n", str_out[i]);
+            //printf("%s\n", str_out[i]);
+        }
+
+    printf("%s\n", str_out[1]);
+    printf("%s\n", str_out[3]);
+    printf("%s\n", str_out[5]);
+    printf("%s\n", str_out[7]);
+
+    for (char *p = strtok(str_out[1], ":,"); p != NULL; p = strtok(NULL, ",")) {
+        printf("%s ", p);
+    }
     return 0;
 }
