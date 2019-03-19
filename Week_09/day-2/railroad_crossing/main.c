799bb8d40d76d7f46d50b876cff5c29966c31a52
znemeth <nemeth.z.a@gmail.com>
2019-03-19 22:01:46 +0100

    add railroad_crossing_sh*tf*ck_done

--- Week_09/day-2/railroad_crossing/main.c
+++ Week_09/day-2/railroad_crossing/main.c
@@ -1,13 +1,52 @@
 #include "stm32f7xx.h"
 #include "stm32746g_discovery.h"
-
 #define UART_PUTCHAR int __io_putchar(int ch)
 
 UART_HandleTypeDef uart_handle;
 GPIO_InitTypeDef user_button_handle;
+TIM_HandleTypeDef LED_timer_handle;
+TIM_HandleTypeDef period_timer_handle;
+
+typedef enum
+{
+	OPEN, SECURING, SECURED, OPENING
+} status_t;
+
+volatile status_t STATUS = OPEN;
+volatile uint8_t counter = 0;
 
 static void Error_Handler(void);
 static void SystemClock_Config(void);
+
+void init_LED_timer()
+{
+	__HAL_RCC_TIM2_CLK_ENABLE()
+	;
+	HAL_TIM_Base_DeInit(&LED_timer_handle);
+	LED_timer_handle.Instance = TIM2;
+	LED_timer_handle.Init.Prescaler = 54000 - 1;
+	LED_timer_handle.Init.Period = 1000 - 1;
+	LED_timer_handle.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+	LED_timer_handle.Init.CounterMode = TIM_COUNTERMODE_UP;
+	HAL_TIM_Base_Init(&LED_timer_handle);
+	HAL_NVIC_SetPriority(TIM2_IRQn, 3, 0);
+	HAL_NVIC_EnableIRQ(TIM2_IRQn);
+}
+
+void init_period_timer()
+{
+	__HAL_RCC_TIM3_CLK_ENABLE()
+	;
+	HAL_TIM_Base_DeInit(&period_timer_handle);
+	period_timer_handle.Instance = TIM3;
+	period_timer_handle.Init.Prescaler = 54000 - 1;
+	period_timer_handle.Init.Period = 2000 - 1;
+	period_timer_handle.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+	period_timer_handle.Init.CounterMode = TIM_COUNTERMODE_UP;
+	HAL_TIM_Base_Init(&period_timer_handle);
+	HAL_NVIC_SetPriority(TIM3_IRQn, 1, 0);
+	HAL_NVIC_EnableIRQ(TIM3_IRQn);
+}
 
 void init_user_button()
 {
@@ -21,7 +60,7 @@ void init_user_button()
 
 	HAL_GPIO_Init(GPIOI, &user_button_handle);
 
-	HAL_NVIC_SetPriority(EXTI15_10_IRQn, 4, 0);
+	HAL_NVIC_SetPriority(EXTI15_10_IRQn, 2, 0);
 
 	HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
 }
@@ -42,19 +81,68 @@ void init_uart()
 
 int main(void)
 {
-
 	HAL_Init();
 	SystemClock_Config();
 	init_uart();
 	init_user_button();
 	BSP_LED_Init(LED_GREEN);
+	init_LED_timer();
+	init_period_timer();
+	HAL_TIM_Base_Start_IT(&LED_timer_handle);
 
+	printf("Entered in OPEN status.\n");
 	while (1) {
 
 	}
 
 	return 0;
 
+}
+
+void TIM2_IRQHandler()
+{
+	HAL_TIM_IRQHandler(&LED_timer_handle);
+}
+
+void TIM3_IRQHandler()
+{
+	HAL_TIM_IRQHandler(&period_timer_handle);
+}
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+	if (htim->Instance == LED_timer_handle.Instance) {
+
+		if (STATUS == OPEN && counter == 1) {
+			BSP_LED_Toggle(LED_GREEN);
+			counter = 0;
+		} else if (STATUS == OPEN) {
+			counter++;
+		} else if (STATUS == SECURING || STATUS == OPENING) {
+			BSP_LED_Toggle(LED_GREEN);
+		} else if (STATUS == SECURED) {
+			BSP_LED_Off(LED_GREEN);
+		}
+	}
+
+	else if (htim->Instance == period_timer_handle.Instance) {
+
+		if (STATUS == SECURING && counter == 5) {
+			counter = 0;
+			STATUS = SECURED;
+			HAL_TIM_Base_Stop_IT(&period_timer_handle);
+			printf("Entered in SECURED status.\n");
+
+		} else if (STATUS == SECURING) {
+			counter++;
+		} else if (STATUS == OPENING && counter == 6) {
+			counter = 0;
+			STATUS = OPEN;
+			printf("Entered in OPEN status.\n");
+		} else if (STATUS == OPENING) {
+			counter++;
+		}
+	}
 }
 
 void EXTI15_10_IRQHandler()
@@ -67,7 +155,16 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 
 	if (GPIO_Pin == user_button_handle.Pin) {
 
-		BSP_LED_Toggle(LED_GREEN);
+		if (STATUS == OPEN) {
+			STATUS = SECURING;
+			printf("Entered in SECURING status.\n");
+			HAL_TIM_Base_Start_IT(&period_timer_handle);
+		} else if (STATUS == SECURED) {
+			STATUS = OPENING;
+			printf("Entered in OPENING status.\n");
+			HAL_TIM_Base_Start_IT(&period_timer_handle);
+		}
+
 	}
 
 }
