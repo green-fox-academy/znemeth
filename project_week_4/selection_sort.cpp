2235318dc5d81fcf1591b3e92ea0061bf65c3199
Zoltan Nemeth <nemeth.z.a@gmail.com>
2019-04-25 10:39:30 +0200

    add_selection_sort with test

--- project_week_4/selection_sort.cpp
+++ project_week_4/selection_sort.cpp
@@ -8,9 +8,6 @@ void swap(int *x, int *y) {
 
 std::vector<int> selectionSort(std::vector<int> list, bool isAscending)
 {
-	double time = (double)cv::getTickCount();
-	int stepCounter = 0;
-
 	for (int i = 0; i < list.size() - 1; i++)
 	{
 		int i_min = i;
@@ -18,22 +15,22 @@ std::vector<int> selectionSort(std::vector<int> list, bool isAscending)
 		{
 			if (list[j] < list[i_min]) {
 				i_min = j;
-				swap(&list[i_min], &list[i]);
-				stepCounter++;
 			}
 		}
+		swap(&list[i_min], &list[i]);
 	}
 
 	if (isAscending == false) {
 		for (int i = 0; i < list.size() / 2; i++) {
 			swap(&list[i], &list[list.size() - 1 - i]);
-			stepCounter++;
 		}
 	}
 
-	time = (double)cv::getTickCount() - time;
-	std::cout << "Number of swaps: " << stepCounter << std::endl;
-	std::cout << "Time for sorting [ms]: " << time << std::endl;
+	for  (int i = 0; i < list.size(); i++)
+	{
+		std::cout << list[i] << std::endl;
+	}
+
 	std::cin.get();
 
 	return list;
